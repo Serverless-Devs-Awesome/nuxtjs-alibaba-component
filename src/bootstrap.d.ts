a47eec4d79c3f89aa814e70bfdbe3338ca44db55
anycodes <jiangyu.ly@alibaba-inc.com>
2020-09-18 23:22:57 +0800

    fix dir

--- src/bootstrap.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport PORT={port}\n{start}";
-export declare const DEFAULTPORT = 9000;
-export declare const DEFAULTSTART = "npx nuxt start --hostname 0.0.0.0 --port $PORT";
-//# sourceMappingURL=bootstrap.d.ts.map
--- src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULTSTART = exports.DEFAULTPORT = exports.DEFAULTBOOTSTRAP = void 0;
-exports.DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport PORT={port}\n{start}";
-exports.DEFAULTPORT = 9000;
-exports.DEFAULTSTART = 'npx nuxt start --hostname 0.0.0.0 --port $PORT';
-//# sourceMappingURL=bootstrap.js.map
