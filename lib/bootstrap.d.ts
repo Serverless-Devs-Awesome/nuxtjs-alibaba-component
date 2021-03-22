a83b57797de8c522022079131561f16853a98846
hanxie <hanxie.wq@alibaba-inc.com>
2021-03-22 15:26:54 +0800

    add lib files

---
+++ lib/bootstrap.d.ts
@@ -0,0 +1,4 @@
+export declare const DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport PORT={port}\n{start}";
+export declare const DEFAULTPORT = 9000;
+export declare const DEFAULTSTART = "npx nuxt start --hostname 0.0.0.0 --port $PORT";
+//# sourceMappingURL=bootstrap.d.ts.map
